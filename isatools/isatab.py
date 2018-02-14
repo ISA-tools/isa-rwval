bd0037e3dc864057c81bb80a46ad67f95ecf894f
Luca Pireddu <pireddu@crs4.it>
2018-02-14 10:49:03 +0100

    Use explicit relative imports to find isatab_* modules
    Seems to be required for Py3 compatibility

--- isatools/isatab.py
+++ isatools/isatab.py
@@ -4,10 +4,10 @@ import logging
 try:
     import numpy as np
     import pandas as pd
-    from isatab_meta import *
-    from isatab_full import *
+    from .isatab_meta import *
+    from .isatab_full import *
 except ImportError as e:
     logging.getLogger(__name__).warn("Missing module")
     logging.getLogger(__name__).warn(str(e))
     logging.getLogger(__name__).warn("Some ISA-tools functionality will not be loaded")
-    from isatab_meta import *
+    from .isatab_meta import *
