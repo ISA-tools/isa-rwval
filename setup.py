aca572bb903e9856aeea0860da0193bb3040c812
djcomlab <djcomlab@gmail.com>
2018-07-18 15:39:36 +0100

    Update version number for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='isa-rwval',
-    version='0.10.6',
+    version='0.10.7',
     packages=['isatools',
               'isatools.errors'
               ],
